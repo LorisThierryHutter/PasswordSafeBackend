34ac52810af8373725a34c581485a824411714ef
Tabea Bolliger <tabea.bolliger@emea.merkleinc.com>
2023-06-22 13:48:59 +0200

    addin them existin files

---
+++ src/test/java/ch/bbw/m183passwordmanagerbackend/M183PasswordManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.m183passwordmanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M183PasswordManagerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
